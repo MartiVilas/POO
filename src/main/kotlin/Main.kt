c177ca7f57cad60dadf65c4af4e74d58067a59da
MartiVilas <marti.vilas14@gmail.com>
2024-01-30 22:09:03 +0100

    creacio class cercle, començament exercici2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,29 +25,5 @@ fun main() {
      * @since 30/01
      */
 
-    var ultimCercle:Cercle
 
-    do {
-        println("1. Crear un cercle d'un radi determinat i d'un color determinat")
-        println("2. Crear un cercle d'un radi determinat")
-        println("3. Mostrar l'ultim cercle creat")
-        println("4. Mostrar el perímetre de l'últim cercle creat")
-        println("5. Mostrar l'àrea de l'últim cercle creat")
-        println("6. Sortir")
-
-        val opcioUsuari: Int = readInt("","Tipus dades incorrecte.",
-            "Valor fora del rang esperat.",1,6 )
-        when(opcioUsuari) {
-            1 -> ultimCercle = crearCercleDeterminat()
-        }
-
-    } while (opcioUsuari != 6)
-}
-
-
-fun crearCercleDeterminat(): Cercle {
-    val radi: Float = llegirFloat("Introdueix el radi desitgat: ", "Tipus dades incorrecte")
-    val color: String = llegirWord("Introdueix el color desitgat: ", "Tipus dades incorrecte")
-
-    return Cercle(radi, color)
 }
