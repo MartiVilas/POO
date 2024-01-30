1c2b7344db59fbff412ac0ecc222e39858b5e1de
MartiVilas <marti.vilas14@gmail.com>
2024-01-30 09:51:55 +0100

    create project 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.10"
+    application
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 group = "org.example"
@@ -7,14 +9,26 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
 }
+tasks.dokkaHtml.configure {
+    outputDirectory.set(project.layout.buildDirectory.dir("dokka"))
+}
+
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+    dependencies {
+        testImplementation(kotlin("test"))
+        testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    }
+
 }
 kotlin {
     jvmToolchain(20)
